9b669e4cb0bd7d80a1177b6487d2ab52bfa51bb0
Khateeb_Rehman <112754572+Khateeb1709@users.noreply.github.com>
2023-10-01 15:32:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here
+let arr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+let regexp =  /\bthe\b|\ban\b|\ba\b/gi;
+
+let articleLessArray = [];
+
+let map = {};  //declaring an object 
+
+for(let i=0; i<arr.length; i++){
+	let articleLessItem = arr[i].replace(regexp, "").trim(); 
+	articleLessArray.push(articleLessItem);
+	mp[articleLessItem] = arr[i];
+}
+
+articleLessArray.sort();
+
+let ans = [];
+
+for(let i of articleLessArray){
+	ans.push(map[i]);
+}
+
+console.log(ans);
+
 
